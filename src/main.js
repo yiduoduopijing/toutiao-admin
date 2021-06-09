05b66ef0d19cc2422cd55d4d92410238bb5537a7
lishaopeng <763292853@qq.com>
2021-06-09 21:19:29 +0800

    第一次提交

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
+// import router from './router'
 import store from './store'
+import './style/index.less'
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
+  // router,
   store,
   render: h => h(App)
 }).$mount('#app')
